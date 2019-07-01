eef6249df7f8763f3459b68f05f5f9d65cca6f40
Pablo Escobar <pescobar001@gmail.com>
2019-07-01 16:40:33 +0200

    added support to add travis or gitlab-ci configs

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# https://github.com/audreyr/cookiecutter/issues/723#issuecomment-350561930
+
+# print(os.getcwd())
+
+import os
+import subprocess
+
+def subprocess_cmd(command):
+    process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
+    proc_stdout = process.communicate()[0].strip()
+    print proc_stdout
+
+if '{{ cookiecutter.add_travis_config }}' == 'n':
+    os.remove('.gitlab-ci.yml')
+
+if '{{ cookiecutter.add_Gitlab_CI_config }}' == 'n':
+    os.remove('.travis.yml')
+
+subprocess_cmd('git init')
+subprocess_cmd('git add .')
+subprocess_cmd('pre-commit install')
+subprocess_cmd('git commit -a -m "Initial commit by cookiecutter"')
+
+print "Path to your new role in the local machine is " + os.getcwd()
--- hooks/post_gen_project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git init &> /dev/null
-git add . &> /dev/null
-pre-commit install &> /dev/null
-git commit -a -m "Initial commit by cookiecutter" &> /dev/null
-
-printf "Path to your new role in the local machine is `pwd` \n"
