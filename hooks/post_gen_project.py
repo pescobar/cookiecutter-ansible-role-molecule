4a96269f4c419ecf6f80f17e2bb3a309112b3ebd
Pablo Escobar <pescobar001@gmail.com>
2019-07-01 16:50:17 +0200

    fix style fix in the python hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,10 +7,12 @@
 import os
 import subprocess
 
+
 def subprocess_cmd(command):
-    process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
     print proc_stdout
+
 
 if '{{ cookiecutter.add_travis_config }}' == 'n':
     os.remove('.gitlab-ci.yml')
